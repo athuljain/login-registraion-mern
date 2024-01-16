036684467aea9d76d693185621e83c6f6a23effd
athuljain <athuljainkj2@gmail.com>
2024-01-16 12:26:55 +0530

    user add cart products backend & frontend again.

--- Back-end/server.js
+++ Back-end/server.js
@@ -10,20 +10,12 @@ app.use(express.json())
 
 
 
-// app.use(cors({
-//   origin: 'http://localhost:3000',  // Replace with your front-end URL
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: 'http://localhost:3000',  // Replace with your front-end URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
-// Custom CORS middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your front-end URL
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 
 app.use(cookieParser())
