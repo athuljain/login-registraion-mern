67ff2904b9202e846ed9bd48a905c3927647e759
athuljain <athuljainkj2@gmail.com>
2024-03-19 11:58:44 +0530

    .env file setups

--- Back-end/middileware/userMiddileware.js
+++ Back-end/middileware/userMiddileware.js
@@ -1,37 +1,30 @@
-
-const jwt = require('jsonwebtoken');
-const cookieParser=require('cookie-parser')
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const userMiddleware = (req, res, next) => {
   //const token = req.cookies.token;
   const token = req.headers.authorization || req.cookies.token;
-
-  console.log('Token in UserMiddleware:', token);
-
+  console.log("Token in UserMiddleware:", token);
   // Check if token exists
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized - No token provided' });
+    return res
+      .status(401)
+      .json({ message: "Unauthorized - No token provided" });
   }
-
   try {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET, {
       ignoreExpiration: true,
     });
-
-    console.log('Decoded Token:', decoded);
-
+    console.log("Decoded Token:", decoded)
     // Attach user information to the request object
     req.user = decoded;
 
     next(); // Proceed to the next middleware or route handler
   } catch (error) {
-    console.error('Error in userMiddleware:', error);
-    res.status(401).json({ message: 'Unauthorized - Invalid token' });
+    console.error("Error in userMiddleware:", error);
+    res.status(401).json({ message: "Unauthorized - Invalid token" });
   }
-
 };
 
 module.exports = userMiddleware;
-
-
